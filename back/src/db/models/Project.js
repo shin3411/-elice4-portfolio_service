71897ddda21a2ba506b9f31a08660f7f81ab2e30
YoungKon Kim <rladudrhs341@khu.ac.kr>
2022-03-16 23:35:08 +0900

    ProjectMVP-back DB 스키마 작성, model layer 작성 완료

---
+++ back/src/db/models/Project.js
@@ -0,0 +1,40 @@
+import { ProjectModel } from "../schemas/project";
+
+class Project {
+    static async create( newProject ){
+        const createdNewProject = await ProjectModel.create(newProject);
+        return createdNewProject;
+    }
+
+    static async findById({ project_id }){
+        const findProject = await ProjectModel.findOne({id: project_id});
+        return findProject;
+    }
+
+    static async findByQuery({ userId, title, description, fromDate, toDate }){        
+        const query = { userId, title, description, fromDate, toDate };
+        
+        const findProject = await ProjectModel.findOne(query);
+        return findProject;
+    }
+
+    static async findAll(){
+        const projects = await ProjectModel.find({});
+        return projects;
+    }
+
+    static async update({ project_id, fieldToUpdate, newValue }){
+        const filter = { id: project_id };
+        const update = { [fieldToUpdate]: newValue };
+        const option = { returnOriginal: false };
+
+        const updatedProject = await ProjectModel.findOneAndUpdate(
+            filter, 
+            update,
+            option
+        );
+        return updatedProject;
+    }
+}
+
+export { Project };
---
+++ back/src/services/projectService.js
@@ -0,0 +1,102 @@
+import { Project } from "../db";
+import { v4 as uuidv4 } from "uuid";
+
+class projectService {
+    static async addProject({ user_id, title, description, fromDate, toDate }){
+        if( !user_id || !title || !description || !fromDate || !toDate ){
+            const errorMessage = 
+             "빠트린 항목이 있습니다. 모두 채워주세요.";
+            return { errorMessage };
+        }
+
+        const query = {
+            userId: user_id,
+            title,
+            description,
+            fromDate,
+            toDate,
+        }
+        
+        const project = await Project.findByQuery(query);
+        //학적 중복
+        if(project){
+            const errorMessage = 
+             "중복된 학적입니다. 다른 학적을 입력해 주세요."
+            return { errorMessage };
+        }
+        const id = uuidv4();
+        const createdNewProject = await Project.create({ ...query, id});
+        createdNewProject.errorMessage = null;
+        
+        
+        return createdNewProject;
+    }
+
+    static async getProject({ project_id }){
+        // db에서 찾지 못한 경우, null 반환 받음
+        const project = await Project.findById({ project_id });
+
+        if(!project){
+            const errorMessage = 
+             "해당 학적이 존재하지 않습니다.";
+            return { errorMessage };
+        }
+
+        return Project;
+    }
+
+    static async setProject({ project_id, toUpdate }){
+        // 우선 해당 id 의 학적이 db에 존재하는지 여부 확인
+        let project = await Project.findById({ project_id });
+
+        // db에서 찾지 못한 경우, 에러 메시지 반환
+        if (!project) {
+        const errorMessage =
+            "해당 학적이 존재하지 않습니다.";
+        return { errorMessage };
+        }
+
+        // 업데이트 대상에 title이 있다면, 즉 title 값이 null 이 아니라면 업데이트 진행
+        if (toUpdate.title) {
+        const fieldToUpdate = "title";
+        const newValue = toUpdate.title;
+        Project = await Project.update({ project_id, fieldToUpdate, newValue });
+        }
+
+        if (toUpdate.description) {
+        const fieldToUpdate = "description";
+        const newValue = toUpdate.description;
+        Project = await Project.update({ project_id, fieldToUpdate, newValue });
+        }
+
+        if (toUpdate.fromDate) {
+        const fieldToUpdate = "fromDate";
+        const newValue = toUpdate.fromDate;
+        Project = await Project.update({ project_id, fieldToUpdate, newValue });
+        }
+
+        if (toUpdate.toDate) {
+        const fieldToUpdate = "toDate";
+        const newValue = toUpdate.toDate;
+        Project = await Project.update({ project_id, fieldToUpdate, newValue });
+        }
+
+        return Project;
+    }
+
+    static async getProjectList({ project_id }){
+        const projects = await Project.findAll();
+        const filteredProjectList = projects
+        .filter(({ userId }) => userId === project_id);
+        
+        if(filteredProjectList.length === 0){
+            const errorMessage =
+             "해당하는 project_id가 없어 Edulist를 줄 수 없습니다."
+            return { errorMessage };
+        }
+
+        return filteredProjectList;
+    }
+}
+
+export { projectService };
