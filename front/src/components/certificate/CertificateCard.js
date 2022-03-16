7165556bb5dc63fb4853a97328753d9018cc0f7c
seon <seonsy44@gmail.com>
2022-03-16 15:08:15 +0900

    certificate 편집 버튼 추가

--- front/src/components/certificate/CertificateCard.js
+++ front/src/components/certificate/CertificateCard.js
@@ -1,18 +1,33 @@
-import React from "react";
-import { Card, Row, Col, Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { Row, Col, Button } from "react-bootstrap";
+import CertificateEditForm from "./CertificateEditForm";
 
 const CertificateCard = ({ certificate, isEditable, setCertificateList }) => {
+  const [isEditing, setIsEditing] = useState(false);
   return (
     <div className="mb-3">
       <Row>
-        <Col>
-          <div>{certificate.title}</div>
-          <div class="text-muted">{certificate.description}</div>
-          <div class="text-muted">{certificate.when_date}</div>
-        </Col>
+        {isEditing ? (
+          <CertificateEditForm
+            certificate={certificate}
+            setCertificateList={setCertificateList}
+            setIsEditing={setIsEditing}
+          />
+        ) : (
+          <Col>
+            <div>{certificate.title}</div>
+            <div class="text-muted">{certificate.description}</div>
+            <div class="text-muted">{certificate.when_date}</div>
+          </Col>
+        )}
+
         {isEditable ? (
           <Col xs={1}>
-            <Button variant="outline-info" size="sm">
+            <Button
+              variant="outline-info"
+              size="sm"
+              onClick={() => setIsEditing(true)}
+            >
               편집
             </Button>
           </Col>
--- front/src/components/certificate/CertificateEditForm.js
+++ front/src/components/certificate/CertificateEditForm.js
@@ -1,7 +1,60 @@
 import React from "react";
+import { Row, Col, Form, Button } from "react-bootstrap";
+import * as Api from "../../api";
 
-const CertificateEditForm = () => {
-  return <></>;
+const CertificateEditForm = ({
+  certificate,
+  setCertificateList,
+  setIsEditing,
+}) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const title = e.target.title.value;
+    const description = e.target.description.value;
+    const when_date = e.target.date.value;
+    console.log(title, description, when_date);
+
+    // Api.put(`certificates/{certificate.id}`, {title, description, when_date})
+  };
+
+  return (
+    <Col>
+      <Form onSubmit={handleSubmit}>
+        <Form.Control
+          className="mt-3"
+          type="text"
+          name="title"
+          defaultValue={certificate.title}
+        />
+        <Form.Control
+          className="mt-3"
+          type="text"
+          name="description"
+          defaultValue={certificate.description}
+        />
+        <Form.Control
+          className="mt-3"
+          type="date"
+          name="date"
+          defaultValue={certificate.when_date}
+        />
+        <Row className="text-center mt-3">
+          <Col>
+            <Button variant="primary" type="submit">
+              확인
+            </Button>
+            <Button
+              className="ms-3"
+              variant="secondary"
+              onClick={() => setIsEditing(false)}
+            >
+              취소
+            </Button>
+          </Col>
+        </Row>
+      </Form>
+    </Col>
+  );
 };
 
 export default CertificateEditForm;
