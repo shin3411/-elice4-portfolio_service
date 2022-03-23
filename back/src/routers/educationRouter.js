949346cf1512f8e2888b3985a1f5d4ad03fd7e10
[4기-1반] 이동준 <0108dongjun@gmail.com>
2022-03-23 11:55:51 +0900

    feat: 백엔드 edu 검색 기능 추가

--- back/src/routers/educationRouter.js
+++ back/src/routers/educationRouter.js
@@ -5,95 +5,123 @@ import is from "@sindresorhus/is";
 const eduRouter = Router();
 
 eduRouter.post('/education/create', login_required, async (req, res, next) => {
-    try{
-        if(!is.nonEmptyObject(req.body)){
+    try {
+        if (!is.nonEmptyObject(req.body)) {
             throw new Error("headers의 Content-Type을 application/json으로 설정해주세요");
         }
 
-        const user_id = req.currentUserId;        
+        const user_id = req.currentUserId;
         const { school, major, position } = req.body;
-        const newEdu = await eduService.addEdu({user_id, school, major, position});
+        const newEdu = await eduService.addEdu({ user_id, school, major, position });
 
-        if(newEdu.errorMessage){
+        if (newEdu.errorMessage) {
             throw new Error(newEdu.errorMessage);
         }
-        
+
         res.status(201).send(newEdu);
 
-    } catch(err) {
+    } catch (err) {
+        next(err);
+    }
+})
+
+//검색
+eduRouter.get('/educations/search', login_required, async (req, res, next) => {
+    try {
+        const query = {}
+        //한글 깨져서 오는것 decode
+        if (req.query.school) {
+            query.school = { $regex: decodeURIComponent(req.query.school) }
+        }
+        if (req.query.major) {
+            query.major = { $regex: decodeURIComponent(req.query.major) }
+        }
+        if (req.query.position) {
+            query.position = { $regex: decodeURIComponent(req.query.position) }
+        }
+
+        if (!(query.school || query.major || query.position)) {
+            throw new Error('쿼리를 정확하게 입력해 주세요.')
+        }
+
+        const edu = await eduService.searchEdu(query);
+
+        res.status(200).send(edu);
+
+    } catch (err) {
         next(err);
     }
 })
 
 eduRouter.get('/educations/:id', login_required, async (req, res, next) => {
-    try{
+    try {
         const edu_id = req.params.id;
         const edu = await eduService.getEdu({ edu_id });
 
-        if(edu.errorMessage){
+        if (edu.errorMessage) {
             throw new Error(edu.errorMessage);
         }
         res.status(200).send(edu);
 
-    } catch(err) {
+    } catch (err) {
         next(err);
     }
 })
 
 eduRouter.put('/educations/:id', login_required, async (req, res, next) => {
-    try{
-        if(!is.nonEmptyObject(req.body)){
+    try {
+        if (!is.nonEmptyObject(req.body)) {
             throw new Error("headers의 Content-Type을 application/json으로 설정해주세요");
         }
 
         const edu_id = req.params.id;
         const eduCheck = await eduService.getEdu({ edu_id });
-        if(eduCheck.userId !== req.currentUserId){
-           throw new Error("다른 유저의 학적을 수정할 수 없습니다!");
+        if (eduCheck.userId !== req.currentUserId) {
+            throw new Error("다른 유저의 학적을 수정할 수 없습니다!");
         }
 
         // 참고로 req.body 가 { school: '학교', major: '전공' }이면,
         // position에 할당되는 값은 undefined 이다!
-        const { school, major, position } = req.body; 
-        const toUpdate = { school, major, position };         
+        const { school, major, position } = req.body;
+        const toUpdate = { school, major, position };
         const edu = await eduService.setEdu({ edu_id, toUpdate });
 
-        if(edu.errorMessage){
+        if (edu.errorMessage) {
             throw new Error(edu.errorMessage);
         }
         res.status(200).send(edu);
 
-    } catch(err) {
+    } catch (err) {
         next(err);
     }
 })
 
 eduRouter.get('/educationlist/:user_id', login_required, async (req, res, next) => {
-    try{
-        const { user_id }= req.params;
+    try {
+        const { user_id } = req.params;
         const eduList = await eduService.getEduList({ user_id });
 
-        if(eduList?.errorMessage){
+        if (eduList?.errorMessage) {
             throw new Error(eduList.errorMessage);
         }
         res.status(200).send(eduList);
 
-    } catch(err) {
+    } catch (err) {
         next(err);
     }
 })
 
 eduRouter.delete('/educations/:id', login_required, async (req, res, next) => {
-    try{
+    try {
         const edu_id = req.params.id;
         const edu = await eduService.deleteEdu({ edu_id });
 
-        if(edu.errorMessage){
+        if (edu.errorMessage) {
             throw new Error(edu.errorMessage);
         }
         res.status(200).send(edu);
 
-    } catch(err) {
+    } catch (err) {
         next(err);
     }
 })
--- back/src/services/educationService.js
+++ back/src/services/educationService.js
@@ -3,10 +3,10 @@ import { v4 as uuidv4 } from "uuid";
 import { User } from "../db";
 
 class eduService {
-    static async addEdu({ user_id, school, major, position }){
-        if( !user_id || !school || !major || !position){
-            const errorMessage = 
-             "빠트린 항목이 있습니다. 모두 채워주세요.";
+    static async addEdu({ user_id, school, major, position }) {
+        if (!user_id || !school || !major || !position) {
+            const errorMessage =
+                "빠트린 항목이 있습니다. 모두 채워주세요.";
             return { errorMessage };
         }
 
@@ -16,77 +16,89 @@ class eduService {
             major,
             position,
         }
-        
+
         const education = await Education.findByQuery(query);
         //학적 중복
-        if(education){
-            const errorMessage = 
-             "중복된 학적입니다. 다른 학적을 입력해 주세요."
+        if (education) {
+            const errorMessage =
+                "중복된 학적입니다. 다른 학적을 입력해 주세요."
             return { errorMessage };
         }
         const id = uuidv4();
-        const createdNewEdu = await Education.create({ ...query, id});
+        const createdNewEdu = await Education.create({ ...query, id });
         createdNewEdu.errorMessage = null;
-        
-        
+
+
         return createdNewEdu;
     }
 
-    static async getEdu({ edu_id }){
+    static async getEdu({ edu_id }) {
         const education = await Education.findById({ edu_id });
-        
-        if(!education){
-            const errorMessage = 
-             "해당 학적이 존재하지 않습니다.";
+
+        if (!education) {
+            const errorMessage =
+                "해당 학적이 존재하지 않습니다.";
             return { errorMessage };
         }
 
         return education;
     }
 
-    static async setEdu({ edu_id, toUpdate }){
+    static async setEdu({ edu_id, toUpdate }) {
         // 우선 해당 id 의 학적이 db에 존재하는지 여부 확인
         let education = await Education.findById({ edu_id });
 
         // db에서 찾지 못한 경우, 에러 메시지 반환
         if (!education) {
-        const errorMessage =
-            "해당 학적이 존재하지 않습니다.";
-        return { errorMessage };
+            const errorMessage =
+                "해당 학적이 존재하지 않습니다.";
+            return { errorMessage };
         }
 
-        for(const [key, value] of Object.entries(toUpdate)){
-            if(!value){
+        for (const [key, value] of Object.entries(toUpdate)) {
+            if (!value) {
                 delete toUpdate[key]
             }
         }
-        
+
         education = await Education.update({ edu_id, toUpdate });
         return education;
     }
 
-    static async getEduList({ user_id }){
+    static async getEduList({ user_id }) {
         const educations = await Education.findAll({ user_id });
-        
-        if(educations.length === 0){
+
+        if (educations.length === 0) {
             const errorMessage =
-             "해당하는 user_id가 없어 Edulist를 줄 수 없습니다."
+                "해당하는 user_id가 없어 Edulist를 줄 수 없습니다."
             return { errorMessage };
         }
 
         return educations;
     }
 
-    static async deleteEdu({ edu_id }){
+    static async deleteEdu({ edu_id }) {
         const education = await Education.deleteById({ edu_id });
-        
-        if(!education){
-            const errorMessage = 
-             "해당 학적이 존재하지 않습니다.";
+
+        if (!education) {
+            const errorMessage =
+                "해당 학적이 존재하지 않습니다.";
             return { errorMessage };
         }
 
         return education;
+    }
+
+    static async searchEdu(query) {
+        const educations = await Education.findsByQuery(query);
+
+        if (educations.length === 0) {
+            const errorMessage =
+                "해당하는 학력이 없습니다."
+            return { errorMessage };
+        }
+
+        return educations;
     }
 }
 
