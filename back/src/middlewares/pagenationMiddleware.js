485f07d4a8d908e99cf99c0a73d1660ea8879950
[4기-1반] 이동준 <0108dongjun@gmail.com>
2022-03-24 11:05:46 +0900

    fix: 백엔드 페이지네이션 마지막 페이지 같이 반환하게 수정

--- back/src/middlewares/pagenationMiddleware.js
+++ back/src/middlewares/pagenationMiddleware.js
@@ -3,30 +3,32 @@ function pagenationMiddleware(req, res, next) {
     const users = req.data;  //req.data는 반드시 배열이어야 한다.
     const { page, limit } = req.query;
 
-    
-    try{
+
+    try {
         //필요한 변수들 선언
-        const total = users.length;  
+        const total = users.length;
         const totalN = typeof total === 'number' ? total : Number(total);
         const pageN = typeof page === 'number' ? page : Number(page);
         const limitN = typeof limit === 'number' ? limit : Number(limit);
-        const lastPage = Math.floor(totalN/limitN) + 1;
-        
-        const startIndex = (pageN-1) * limitN;
-        if(startIndex > total - 1){
+        const lastPage = Math.floor(totalN / limitN) + 1;
+
+        const startIndex = (pageN - 1) * limitN;
+        if (startIndex > total - 1) {
             const errorMessage =
-            "해당 페이지는 이미 전체 개수를 넘었습니다.";
+                "해당 페이지는 이미 전체 개수를 넘었습니다.";
             throw new Error(errorMessage);
         }
         const lastIndex = lastPage === pageN ? totalN - 1 : pageN * limitN - 1;
 
         const pagedUsers = [];
-        for(let i=startIndex; i<=lastIndex; i++){
-        pagedUsers.push(users[i]);
+        for (let i = startIndex; i <= lastIndex; i++) {
+            pagedUsers.push(users[i]);
         }
 
-        res.status(200).send(pagedUsers);
-    } catch(error) {
+        const ret = { lastPage: lastPage, data: pagedUsers }
+
+        res.status(200).send(ret);
+    } catch (error) {
         next(error);
     }
 }
