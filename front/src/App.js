131bead55e0b01c636afa2de76290577923c0a08
bgeun <papoke22@gmail.com>
2022-03-25 21:46:29 +0900

    fix: 로그인시 방명록 수정/삭제 버그 수정

--- front/src/App.js
+++ front/src/App.js
@@ -13,9 +13,8 @@ import RegisterForm from "./components/user/RegisterForm";
 import Portfolio from "./components/Portfolio";
 import Search from "./components/search/Search";
 
-import { useRecoilValue, useSetRecoilState } from "recoil";
+import { useRecoilValue } from "recoil";
 import { modeState } from "./atom/themeState";
-import loginIdState from "./atom/loginIdState";
 
 export const UserStateContext = createContext(null);
 export const DispatchContext = createContext(null);
@@ -28,7 +27,6 @@ function App() {
   const [userState, dispatch] = useReducer(loginReducer, {
     user: null,
   });
-  const setLogIdState = useSetRecoilState(loginIdState);
 
   // 아래의 fetchCurrentUser 함수가 실행된 다음에 컴포넌트가 구현되도록 함.
   // 아래 코드를 보면 isFetchCompleted 가 true여야 컴포넌트가 구현됨.
@@ -40,7 +38,6 @@ function App() {
       // 이전에 발급받은 토큰이 있다면, 이를 가지고 유저 정보를 받아옴.
       const res = await Api.get("user/current");
       const currentUser = res.data;
-      setLogIdState(currentUser.id);
 
       // dispatch 함수를 통해 로그인 성공 상태로 만듦.
       dispatch({
--- front/src/components/Portfolio.js
+++ front/src/components/Portfolio.js
@@ -12,6 +12,9 @@ import Certificates from "./certificate/Certificates";
 import Projects from "./project/Projects";
 import Educations from "./education/Educations";
 
+import { useSetRecoilState } from "recoil";
+import loginIdState from "../atom/loginIdState";
+
 function Portfolio() {
   const navigate = useNavigate();
   const params = useParams();
@@ -21,13 +24,23 @@ function Portfolio() {
   // 아래 코드를 보면, isFetchCompleted가 false이면 "loading..."만 반환되어서, 화면에 이 로딩 문구만 뜨게 됨.
   const [isFetchCompleted, setIsFetchCompleted] = useState(false);
   const userState = useContext(UserStateContext);
+  const setLoginIdState = useSetRecoilState(loginIdState);
+
+  // 로그인 후 유저의 id를 전역으로 저장하기 위한 코드
+  useEffect(() => {
+    const firstIdFetch = async () => {
+      const res = await Api.get("users", userState.user.id);
+      const currentId = res.data.id;
+      setLoginIdState(currentId);
+    };
+    firstIdFetch();
+  }, []);
 
   const fetchPorfolioOwner = async (ownerId) => {
     // 유저 id를 가지고 "/users/유저id" 엔드포인트로 요청해 사용자 정보를 불러옴.
     const res = await Api.get("users", ownerId);
     // 사용자 정보는 response의 data임.
     const ownerData = res.data;
-    console.log(res.data);
     // portfolioOwner을 해당 사용자 정보로 세팅함.
     setPortfolioOwner(ownerData);
     // fetchPorfolioOwner 과정이 끝났으므로, isFetchCompleted를 true로 바꿈.
